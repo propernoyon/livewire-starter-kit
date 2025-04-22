99143ec7fae859816a51fc6b3c4536e46e2b113a
Tony Lea <tony@devdojo.com>
2025-04-22 10:18:31 -0400

    Adding update

--- resources/views/components/input-otp.blade.php
+++ resources/views/components/input-otp.blade.php
@@ -1,100 +1,88 @@
 @props([
     // total number of boxes to display
     'digits' => 4,
-    'eventCallback' => null
+    'eventCallback' => null,
 ])
 
-<div x-data="
-    {
-        total_digits: @js($digits),
-        eventCallback: @js($eventCallback),
-        moveCursorNext (index, digits, evt) {
-            if (!isNaN(parseInt(evt.key)) && parseInt(evt.key) >= 0 && parseInt(evt.key) <= 9 && index != digits) {
+<div x-data="{
+    total_digits: @js($digits),
+    eventCallback: @js($eventCallback),
+    moveCursorNext(index, digits, evt) {
+        if (!isNaN(parseInt(evt.key)) && parseInt(evt.key) >= 0 && parseInt(evt.key) <= 9 && index != digits) {
+            evt.preventDefault();
+            evt.stopPropagation();
+            this.$refs['input' + index].value = evt.key;
+            this.$refs['input' + (index + 1)].focus();
+        } else {
+            if (evt.key === 'Backspace') {
                 evt.preventDefault();
                 evt.stopPropagation();
-                this.$refs['input' + index].value = evt.key;
-                this.$refs['input' + (index+1)].focus();
-            } else {
-                if (evt.key === 'Backspace') {
-                    evt.preventDefault();
-                    evt.stopPropagation();
-                    if (index > 1) {
-                        if (this.$refs['input' + index].value !== '') {
-                            this.$refs['input' + index].value = '';
-                        } else {
-                            if (index > 1) {
-                                this.$refs['input' + (index-1)].value='';
-                                this.$refs['input' + (index-1)].focus();
-                            }
-                        }
-                    } else {
+                if (index > 1) {
+                    if (this.$refs['input' + index].value !== '') {
                         this.$refs['input' + index].value = '';
+                    } else {
+                        if (index > 1) {
+                            this.$refs['input' + (index - 1)].value = '';
+                            this.$refs['input' + (index - 1)].focus();
+                        }
                     }
-                } 
-            }
-            let that = this;
-            setTimeout(function(){
-                that.$refs.pin.value = that.generateCode();
-                if (index === digits && [...Array(digits).keys()].every(i => that.$refs['input' + (i + 1)].value !== '')) {
-                    that.submitCallback();
-                }
-            }, 100);
-        },
-        submitCallback(){
-            if(this.eventCallback){
-                window.dispatchEvent(new CustomEvent(this.eventCallback, { detail: { code: this.generateCode() }}));
-            }
-        },
-        pasteValue(event){
-            event.preventDefault();
-            let paste = (event.clipboardData || window.clipboardData).getData('text');
-            for (let i = 0; i < paste.length; i++) {
-                if (i < this.total_digits) {
-                    this.$refs['input' + (i + 1)].value = paste[i];
-                }
-                let focusLastInput = (paste.length <= this.total_digits) ? paste.length : this.total_digits;
-                this.$refs['input' + focusLastInput].focus();
-                if(paste.length >= this.total_digits){
-                    let that = this;
-                    setTimeout(function(){
-                        that.$refs.pin.value = that.generateCode();
-                        that.submitCallback();
-                    }, 100);
-                    
+                } else {
+                    this.$refs['input' + index].value = '';
                 }
             }
-        },
-        generateCode() {
-            let code = '';
-            for (let i = 1; i <= this.total_digits; i++) {
-                code += this.$refs['input' + i].value;
+        }
+        let that = this;
+        setTimeout(function() {
+            that.$refs.pin.value = that.generateCode();
+            if (index === digits && [...Array(digits).keys()].every(i => that.$refs['input' + (i + 1)].value !== '')) {
+                that.submitCallback();
             }
-            return code;
-        },
-    }" 
-    x-init="
-        setTimeout(function(){
-            $refs.input1.focus();
         }, 100);
-    "
-    @focus-auth-2fa-auth-code.window="$refs.input1.focus()"
-    class="relative input-otp"
->
+    },
+    submitCallback() {
+        if (this.eventCallback) {
+            window.dispatchEvent(new CustomEvent(this.eventCallback, { detail: { code: this.generateCode() } }));
+        }
+    },
+    pasteValue(event) {
+        event.preventDefault();
+        let paste = (event.clipboardData || window.clipboardData).getData('text');
+        for (let i = 0; i < paste.length; i++) {
+            if (i < this.total_digits) {
+                this.$refs['input' + (i + 1)].value = paste[i];
+            }
+            let focusLastInput = (paste.length <= this.total_digits) ? paste.length : this.total_digits;
+            this.$refs['input' + focusLastInput].focus();
+            if (paste.length >= this.total_digits) {
+                let that = this;
+                setTimeout(function() {
+                    that.$refs.pin.value = that.generateCode();
+                    that.submitCallback();
+                }, 100);
+
+            }
+        }
+    },
+    generateCode() {
+        let code = '';
+        for (let i = 1; i <= this.total_digits; i++) {
+            code += this.$refs['input' + i].value;
+        }
+        return code;
+    },
+}" x-init="setTimeout(function() {
+    $refs.input1.focus();
+}, 100);" @focus-auth-2fa-auth-code.window="$refs.input1.focus()"
+    class="relative input-otp">
     <div class="flex">
         <div class="flex mx-auto space-x-2">
             @for ($x = 1; $x <= $digits; $x++)
-                <input
-                    x-ref="input{{ $x }}"
-                    type="text"
-                    inputmode="numeric"
-                    pattern="[0-9]"
+                <input x-ref="input{{ $x }}" type="text" inputmode="numeric" pattern="[0-9]"
                     x-on:paste="pasteValue"
                     x-on:keydown="moveCursorNext({{ $x }}, {{ $digits }}, event)"
-                    x-on:focus="$el.select()"
-                    x-on:input="$el.value = $el.value.replace(/[^0-9]/g, '').slice(0, 1)"
+                    x-on:focus="$el.select()" x-on:input="$el.value = $el.value.replace(/[^0-9]/g, '').slice(0, 1)"
                     class="w-12 h-12 font-light text-center text-black dark:text-stone-100 rounded-md border shadow-sm appearance-none auth-component-code-input dark:text-dark-400 border-stone-200 dark:border-stone-700 focus:border-2"
-                    maxlength="1"
-                />
+                    maxlength="1" />
             @endfor
         </div>
     </div>
